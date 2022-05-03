e8cb76fa26f88830c1eb19bf5aa7ed6232b4b53c
Tharindu Rajapaksha <tharindu.tharaka.rajapaksha@gmail.com>
2022-05-03 22:49:58 +0530

    Login interface commit

---
+++ hydro/lib/main.dart
@@ -0,0 +1,182 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hydro/profile_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+/*
+
+step 1 : create the main layout of the app(only the UI)
+step 2 : Login to your firebase console
+step 3 : create a new project
+step 4 : Add the firebase depenedeicie for Flutter
+step 5 : lets init the firebase app
+step 6 : create the login function
+step 7 : create a user inside the firebase and test the app
+step 8 : let's input our user data and test the app
+*/
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //initialialze firebase app...............................................
+
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return LoginScreen();
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //Login function
+  static Future<User?> loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No User found for that email");
+      }
+    }
+
+    return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //creat the textfiled controller
+
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            "MyApp Title",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            "Login to Your App",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "User Email",
+              prefixIcon: Icon(Icons.mail, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: "User Password",
+              prefixIcon: Icon(Icons.lock, color: Colors.black),
+            ),
+          ),
+          const Text(
+            "Don't Remember your Password",
+            style: TextStyle(color: Colors.blue),
+          ),
+          const SizedBox(
+            height: 88.0,
+          ),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)),
+              onPressed: () async {
+                //let's test app
+                User? user = await loginUsingEmailPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context);
+                print(user);
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => ProfileScreen()));
+                  //let's make a new screen
+                }
+              },
+              child: const Text("Login",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ hydro/lib/profile_screen.dart
@@ -0,0 +1,116 @@
+/*
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Welcome tou you profile page"),
+      ),
+    );
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+import 'package:circular/circular.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(body: Center(child: Home())),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int angle = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: const Color(0xffEEEEEE),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: 200, //! this should not be less than
+                height: 200,
+                child: CircularSlider(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(200),
+                      boxShadow: const [
+                        BoxShadow(
+                            offset: Offset(-10, -10),
+                            color: Colors.white,
+                            blurRadius: 20,
+                            spreadRadius: 1),
+                        BoxShadow(
+                            offset: Offset(10, 10),
+                            color: Color.fromARGB(255, 158, 158, 158),
+                            blurRadius: 20,
+                            spreadRadius: 1)
+                      ]),
+                  maxValue: 100,
+                  radius:
+                      100, //! this one multiplied by 2  ( sized box width and height >= radius * 2)
+                  child: Text(
+                    angle.toString(),
+                    style: const TextStyle(fontSize: 30),
+                  ),
+                  color: const Color(0xffEEEEEE),
+                  sliderColor: const Color(0xff62CBDA),
+                  unSelectedColor: const Color(0xffD7DEE7),
+                  onDrag: (angle) {
+                    setState(() {
+                      this.angle = angle;
+                    });
+                  },
+                ),
+              ),
+              CircularViewer(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(200),
+                    boxShadow: const [
+                      BoxShadow(
+                          offset: Offset(-10, -10),
+                          color: Colors.white,
+                          blurRadius: 20,
+                          spreadRadius: 1),
+                      BoxShadow(
+                          offset: Offset(10, 10),
+                          color: Color.fromARGB(255, 158, 158, 158),
+                          blurRadius: 20,
+                          spreadRadius: 1)
+                    ]),
+                value: 50,
+                maxValue: 100,
+                radius: 100,
+                textStyle: const TextStyle(fontSize: 30),
+                color: const Color(0xffEEEEEE),
+                sliderColor: const Color(0xff62CBDA),
+                unSelectedColor: const Color(0xffD7DEE7),
+              )
+            ],
+          ),
+          alignment: Alignment.center,
+        ),
+      ),
+    );
+  }
+}
